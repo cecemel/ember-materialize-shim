b0657491cbad03b3e7a04403883a11135f58d48b
cecemel <ruiz.felixruiz@gmail.com>
2017-04-15 19:59:34 +0200

    Update index.js

--- blueprints/ember-materialize-shim/index.js
+++ blueprints/ember-materialize-shim/index.js
@@ -4,7 +4,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   beforeInstall: function() {
-    return this.addBowerPackageToProject('materialize', '0.98.1').then(function() {
+    return this.addBowerPackageToProject('materialize', '0.98.2').then(function() {
       return this.addAddonsToProject({
         packages: [{
           name: 'ember-cli-sass',
